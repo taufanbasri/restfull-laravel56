3516e1a31292bcc2bc17610f8f11146d84a1101b
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-23 06:26:38 +0700

    Using Global Scopes

--- app/Buyer.php
+++ app/Buyer.php
@@ -1,9 +1,17 @@
 <?php
 
 namespace App;
+use App\Scopes\BuyerScope;
 
 class Buyer extends User
 {
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new BuyerScope);
+    }
+
     public function transactions()
     {
       return $this->hasMany(Transaction::class);
--- app/Seller.php
+++ app/Seller.php
@@ -1,9 +1,17 @@
 <?php
 
 namespace App;
+use App\Scopes\SellerScope;
 
 class Seller extends User
 {
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new SellerScope);
+    }
+
     public function products()
     {
       return $this->hasMany(Product::class);
