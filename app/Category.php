b1e94d0358a4b3fc67b8ce5de84c1e72bfe6c3f9
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-22 11:12:40 +0700

    Implementing Relationships Between Models

--- app/Category.php
+++ app/Category.php
@@ -9,4 +9,9 @@ class Category extends Model
     protected $fillable = [
         'name', 'description'
     ];
+
+    public function products()
+    {
+      return $this->belongsToMany(Product::class);
+    }
 }
