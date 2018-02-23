7af8b9e22ec15b031df0022dd8fa49689f82ee51
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-23 20:06:45 +0700

    Laravel Mail for User Created

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Mail;
+use App\User;
 use App\Product;
+use App\Mail\UserCreated;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        User::created(function ($user) {
+            Mail::to($user)->send(new UserCreated($user));
+        });
+
         Product::updated(function ($product) {
             if ($product->quantity == 0 && $product->isAvailable()) {
                 $product->status = Product::UNAVAILABLE_PRODUCT;
