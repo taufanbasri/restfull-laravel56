fe611fe39fdd63a0609e6dca6386bdabc949ffbe
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-24 19:04:59 +0700

    Returning HTML when required

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -152,6 +152,10 @@ class Handler extends ExceptionHandler
     {
         $errors = $e->validator->errors()->getMessages();
 
+        if ($this->isFrontend($request)) {
+            return $request->ajax() ? response()->json($errors, 422) : redirect()->back()->withInput($request->input())->withErrors($errors);
+        }
+
         return $this->errorResponse($errors, 422);
     }
 
@@ -164,6 +168,15 @@ class Handler extends ExceptionHandler
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
+        if ($this->isFrontend($request)) {
+            return redirect()->guest('login');
+        }
+
         return $this->errorResponse('Unauthenticated', 401);
+    }
+
+    private function isFrontend($request)
+    {
+        return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
     }
 }
