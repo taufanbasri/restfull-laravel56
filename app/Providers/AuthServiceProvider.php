91e5cdef860ef010cec059462cd6b67760a17ba4
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-25 13:01:58 +0700

    Preparing Scopes for API

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,5 +31,12 @@ class AuthServiceProvider extends ServiceProvider
         // Passport::tokensExpireIn(Carbon::now()->addDays(15));
         // Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
         Passport::enableImplicitGrant();
+
+        Passport::tokensCan([
+            'purchase-product' => 'Create a new transaction for a specipic product',
+            'manage-product' => 'Create, read, update, and delete product (CRUD)',
+            'manage-account' => 'Read your account data, id, name, email, if verified, and if admin (cannot read password). Modify your account data(email and password). Cannot delete your account',
+            'read-general' => 'Read general information like purchasing categories, purchased products, selling products, selling categories, your transactions (purchases and sales)',
+        ]);
     }
 }
