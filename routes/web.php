8d4d5f848f4723c1e3de58d6f1e827e79aa006eb
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-02-25 12:43:06 +0700

    Using Laravel Passport

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,10 @@ $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 $this->post('password/reset', 'Auth\ResetPasswordController@reset');
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/**
+ * tokens management
+ */
+Route::get('/home/authorized-clients', 'HomeController@getAuthorizedClients')->name('authorized-clients');
+Route::get('/home/my-clients', 'HomeController@getClients')->name('personal-clients');
+Route::get('/home/my-tokens', 'HomeController@getTokens')->name('personal-tokens');
