9d2706f5d274e2ea63b6a88c239af7ce1f362858
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2019-09-26 19:39:54 +0700

    Identifying Authenticed User

--- routes/api.php
+++ routes/api.php
@@ -59,6 +59,7 @@ Route::resource('transactions.sellers', 'Transaction\TransactionSellerController
 /**
  * Users
  */
+Route::name('me')->get('users/me', 'User\UserController@me');
 Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
 Route::get('users/verify/{token}', 'User\UserController@verify')->name('verify');
 Route::get('users/{user}/resend', 'User\UserController@resend')->name('resend');
